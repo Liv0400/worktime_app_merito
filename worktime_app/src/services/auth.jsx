a4f355ecd67128ca96cb6a0072dbc2f68169d388
LiwiaB <liwia_fructis@onet.pl>
2024-05-24 22:11:32 +0200

    Update from DEV to liwia_b

--- worktime_app/src/services/auth.jsx
+++ worktime_app/src/services/auth.jsx
@@ -1,77 +1,85 @@
 import { auth } from "./firebase";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { createUserData } from "./firestore";
+import { getFirestore } from "firebase/firestore";
 
-export const getCurrentUser =() => {
-return new Promise((resolve, reject)=>{
-    const unsub = onAuthStateChanged( auth, (user)=>{
-        unsub()
+const firestore = getFirestore();
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsub = onAuthStateChanged(
+      auth,
+      (user) => {
+        unsub();
         if (user) {
-            resolve(user);
+          resolve(user);
         } else {
-            resolve(null);
+          resolve(null);
         }
-        
-    }, reject)
-}
-)  
-}
+      },
+      reject
+    );
+  });
+};
 
-
-<<<<<<< HEAD
-export const signUpUser = async ( { fullname, email, password, firstName, lastName 
-=======
-export const signUpUser = async ( { fullname, email, password, firstName, lastName //,typedeal
->>>>>>> f836a97539fecca55a1a2c5f8238bea6bd50df8d
-
+export const signUpUser = async ({
+  fullname,
+  email,
+  password,
+  firstName,
+  lastName, //,typedeal
 }) => {
-    try {
-        const result = await createUserWithEmailAndPassword(auth, email, password );
-        const user = result.user;
+  try {
+    const result = await createUserWithEmailAndPassword(auth, email, password);
+    const user = result.user;
 
-        if(user){
-            await updateProfile(user, {
-              displayName: `${firstName} ${lastName}`,
-        });
-            await createUserData({
-             uid: result.user.uid,
-<<<<<<< HEAD
-             fullname, 
-=======
-             fullname, //, typedeal
->>>>>>> f836a97539fecca55a1a2c5f8238bea6bd50df8d
-         });
-         return user;
-        } else {
-            throw new Error("Użytkownik jest null lub undefined");
-        }
-    } catch (error) {
-        console.error('Error during sign up:', error);
-        return null;
+    if (user) {
+      await updateProfile(user, {
+        displayName: `${firstName} ${lastName}`,
+      });
+      await createUserData({
+        uid: result.user.uid,
+
+        fullname, //, typedeal
+      });
+      return user;
+    } else {
+      throw new Error("Użytkownik jest null lub undefined");
     }
-    
+  } catch (error) {
+    console.error("Error during sign up:", error);
+    return null;
+  }
+};
 
-}
-
-export const signInUser = async ({email, password})=>{
-try {
-    await signInWithEmailAndPassword(auth, email, password)
-    return true
-} catch (error) {
-    return false
-}
-}
+export const signInUser = async ({ email, password }) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
 export const updateUser = async (userId, userData) => {
-    try {
-      await firestore.collection('users').doc(userId).set(userData, { merge: true });
-      return true;
-    } catch (error) {
-      console.error("Error updating user:", error);
-      throw error;
-    }
-  };
+  try {
+    await firestore
+      .collection("users")
+      .doc(userId)
+      .set(userData, { merge: true });
+    return true;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    throw error;
+  }
+};
 
-export const logout = async () =>{
-    await signOut (auth)
-}
+export const logout = async () => {
+  await signOut(auth);
+};
