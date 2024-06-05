136c0626c6611ceff24a7078d8f90349acd02a49
JustynaSzuba <szuba.justyna@wp.pl>
2024-06-05 19:23:56 +0200

    Zmiana formatu daty

--- worktime_app/src/components/pages/NowyWniosek.jsx
+++ worktime_app/src/components/pages/NowyWniosek.jsx
@@ -26,13 +26,13 @@ export const NowyWniosek = () => {
     fetchCurrentUser();
   }, []);
 
-  const formatDate = (date) => {
-  const d = new Date(date);
-  const day = String(d.getDate()).padStart(2, '0');
-  const month = String(d.getMonth() + 1).padStart(2, '0'); // Miesiące są indeksowane od 0
-  const year = d.getFullYear();
-  return `${day}/${month}/${year}`;
-};
+//   const formatDate = (date) => {
+//   const d = new Date(date);
+//   const day = String(d.getDate()).padStart(2, '0');
+//   const month = String(d.getMonth() + 1).padStart(2, '0'); // Miesiące są indeksowane od 0
+//   const year = d.getFullYear();
+//   return `${day}/${month}/${year}`;
+// };
 
   const handleSubmit = async (e) => {
       e.preventDefault();
@@ -59,8 +59,8 @@ export const NowyWniosek = () => {
           userId:currentUserData?.uid || 'ID pracownika',
           name:displayName,
           type: type,
-          beginningDate: formatDate(beginningDate),
-          endingDate: formatDate(endingDate),
+          beginningDate: beginningDate,
+          endingDate: endingDate,
           beginningHour: beginningHour,
           endingHour: endingHour,
           status: 'oczekujący',
